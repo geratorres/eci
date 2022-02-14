ead40cd474f7680139b28d5e8f152a75610f3baf
Gerardo Torres Ontiveros <GerardoTorres@EDCMXENLT0230.local>
2022-02-14 10:09:50 -0700

    initial commit

---
+++ src/test/java/com/encora/eci/EciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.eci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
